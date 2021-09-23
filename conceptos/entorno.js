dbff2cf4f351043f2c3cb8fb1ed0fca153591639
jhovan <jhovan.solano@gmail.com>
2021-09-23 15:14:54 -0500

    Modulo 01 - Clase 01 - 06 | Reacomodo

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -21,7 +21,7 @@ se ejecutan en lugares diferentes, dificultando el despliegue.
 
 let nombre = process.env.NOMBRE || 'Sin nombre';
 let web= process.env.MI_WEB || 'Sin Web';
-const apikey = process.env.APIKEY || 'YUFVGIUGIU87DS99';
+const apikey = process.env.APIKEY || 'YUFMIMIIUGIU87DS99';
 
 console.log(`Hola ${nombre}!`);
 console.log(`Mi web es: ${web}`);
